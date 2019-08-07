00fa75c74b5e51d62b3c181f657e86ebf87e7f84
Thomas Trageser <thomast74@gmail.com>
2019-08-07 23:40:00 +0300

    Make cordova 9 compliant

--- scripts/on-uninstall.js
+++ scripts/on-uninstall.js
@@ -51,7 +51,7 @@ function attempt(fn) {
 }
 
 module.exports = function (ctx) {
-    deferral = ctx.requireCordovaModule('q').defer();
+    deferral = require('q').defer();
     attempt(run)();
     return deferral.promise;
 };
